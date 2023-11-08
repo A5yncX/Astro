7f1595687d2e529d80199cad999088e560594726
AsyncX <1158271328@qq.com>
2023-11-08 19:43:58 +0800

    change color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ export default {
 				bgColor: "hsl(var(--theme-bg) / <alpha-value>)",
 				textColor: "hsl(var(--theme-text) / <alpha-value>)",
 				link: "hsl(var(--theme-link) / <alpha-value>)",
-				accent: "#a0e1ff / <alpha-value>)",
+				accent: "#5d8497 / <alpha-value>)",
 				"accent-2": "hsl(var(--theme-accent-2) / <alpha-value>)",
 				quote: "hsl(var(--theme-quote) / <alpha-value>)",
 			},
