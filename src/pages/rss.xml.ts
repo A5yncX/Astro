6badc489b75ad4e5e67d944f9ed9c25aee68b5a5
AsyncX <1158271328@qq.com>
2023-11-29 17:01:17 +0800

    change rss description

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -10,10 +10,12 @@ export const GET = async () => {
 
 	return rss({
 		title: siteConfig.title,
+		description: siteConfig.title,
 		// description: siteConfig.description,
 		site: import.meta.env.SITE,
 		items: posts.map((post) => ({
 			title: post.data.title,
+			description: post.data.title,
 			// description: post.data.description,
 			pubDate: post.data.publishDate,
 			link: `posts/${post.slug}`,
