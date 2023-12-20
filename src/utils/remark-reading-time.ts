b12b2f041c3dfc8c969a8e07effbd1db740d548e
AsyncX <1158271328@qq.com>
2023-12-20 12:56:48 +0800

    try

--- src/utils/remark-reading-time.ts
+++ src/utils/remark-reading-time.ts
@@ -2,7 +2,7 @@ import getReadingTime from "reading-time";
 import { toString } from "mdast-util-to-string";
 
 export function remarkReadingTime() {
-	return function (tree, { data }) {
+	return function (tree: any, { data }: { data: any }) {
 		const textOnPage = toString(tree);
 		const readingTime = getReadingTime(textOnPage);
 		data.astro.frontmatter.minutesRead = readingTime.text;
