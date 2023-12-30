a486a1b59a8e2951671d04103873823cb700601f
AsyncX <1158271328@qq.com>
2023-12-30 11:36:59 +0800

    add rss img

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,24 +3,26 @@ import { siteConfig } from "@/site-config";
 import { getAllPosts } from "@/utils";
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
+
 const parser = new MarkdownIt();
 
 export const GET = async () => {
-	const posts = await getAllPosts();
-	// 将文章按照时间降序排列
-	posts.sort((a, b) => new Date(b.data.publishDate) - new Date(a.data.publishDate));
+  const posts = await getAllPosts();
+  // 将文章按照时间降序排列
+  posts.sort((a, b) => new Date(b.data.publishDate) - new Date(a.data.publishDate));
 
-	return rss({
-		title: siteConfig.title,
-		description: siteConfig.title,
-		site: import.meta.env.SITE,
-		items: posts.map((post) => ({
-			title: post.data.title,
-			description: post.data.title,
-			pubDate: post.data.publishDate,
-			link: `posts/${post.slug}`,
-			content: sanitizeHtml(parser.render(post.body)),
-		})),
-	});
+  return rss({
+    title: siteConfig.title,
+    description: siteConfig.title,
+    site: import.meta.env.SITE,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      description: post.data.title,
+      pubDate: post.data.publishDate,
+      link: `posts/${post.slug}`,
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
+    })),
+  });
 };
-
